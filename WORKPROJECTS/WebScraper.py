3bc5b2bfbb9ace44e499ec561fb1c1e9d67e3e4d
unknown <biostevedean@gmail.com>
2022-09-01 22:17:04 -0500

    Made it easier to input data.

--- WORKPROJECTS/WebScraper.py
+++ WORKPROJECTS/WebScraper.py
@@ -56,8 +56,10 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser(description='User can input what url header as to where it wants the program to '
                                                  'search for houses')
-    parser.add_argument('--header', help='User can enter a header for the program to search through', type=str,
+    parser.add_argument('--header', help='User can enter a header for the program to search through, enter state('
+                                         'abbreve)/city', type=str,
                         required=False)
+
     args = parser.parse_args()
     assert args.header is None or isinstance(args.header, str)
     if args.header:
